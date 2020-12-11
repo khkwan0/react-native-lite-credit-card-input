805614353f7f37b46afc1772bddd16d85e863336
khkwan0 <khkwan0@gmail.com>
2020-12-11 09:31:31 +0000

    prep files for npm submission

--- LiteCreditCardInput.js
+++ LiteCreditCardInput.js
@@ -3,24 +3,6 @@ import {View, TextInput, Image} from 'react-native'
 import valid from 'card-validator'
 import Images from './images/index'
 
-// the regular expressions check for possible matches as you type, hence the OR operators based on the number of chars
-// regexp string length {0} provided for soonest detection of beginning of the card numbers this way it could be used for BIN CODE detection also
-
-//JCB
-const jcb_regex = new RegExp('^(?:2131|1800|35)[0-9]{0,}$'); //2131, 1800, 35 (3528-3589)
-// American Express
-const amex_regex = new RegExp('^3[47][0-9]{0,}$'); //34, 37
-// Diners Club
-const diners_regex = new RegExp('^3(?:0[0-59]{1}|[689])[0-9]{0,}$'); //300-305, 309, 36, 38-39
-// Visa
-const visa_regex = new RegExp('^4[0-9]{0,}$'); //4
-// MasterCard
-const mastercard_regex = new RegExp('^(5[1-5]|222[1-9]|22[3-9]|2[3-6]|27[01]|2720)[0-9]{0,}$'); //2221-2720, 51-55
-const maestro_regex = new RegExp('^(5[06789]|6)[0-9]{0,}$'); //always growing in the range: 60-69, started with / not something else, but starting 5 must be encoded as mastercard anyway
-//Discover
-const discover_regex = new RegExp('^(6011|65|64[4-9]|62212[6-9]|6221[3-9]|622[2-8]|6229[01]|62292[0-5])[0-9]{0,}$')
-////6011, 622126-622925, 644-649, 65
-
 const LiteCreditCardInput = props => {
 
   const [brand, setBrand] = React.useState('unknown')
